47e1240394967388f84722209d453894979d189d
mamamamad <mamadhub8@gmail.com>
2025-08-19 19:41:22 +0330

    add front URL for CORS

--- Application.mjs
+++ Application.mjs
@@ -59,7 +59,7 @@ class Application {
   async #initCors() {
     this.#app.use(
       cors({
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "http://localhost:3001"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
