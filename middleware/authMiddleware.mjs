0f4723a0bb0677822d4d8e9d5e4d49118543c15e
mamamamad <mamadhub8@gmail.com>
2025-08-20 16:41:44 +0330

    fix the logout problem with jwt.

--- middleware/authMiddleware.mjs
+++ middleware/authMiddleware.mjs
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import { getEnv } from "../core/utils.mjs";
 import { UserModel } from "../globalMoudles.mjs";
 import { log } from "../core/utils.mjs";
+import redis from "../core/redis.mjs";
+import crypto from "../core/crypto.mjs";
 
 export async function authJwt(req, res, next) {
   try {
@@ -9,11 +11,16 @@ export async function authJwt(req, res, next) {
     if (!token) return res.status(401).json({ error: "Access denied" });
     const secretKey = getEnv("SECRET_KEY_JWT");
     const decoded = jwt.verify(token, secretKey);
-    const userId = await UserModel.userExistEmail(decoded.email);
-
-    req.userId = userId[0].email;
-    next();
+    const base64Code = crypto.stringtoBase64(decoded.email);
+    let existRefreshToken = await redis.redis1.ftSearchUserTokenIdEmail(
+      base64Code
+    );
+    if (base64Code === existRefreshToken[2][5]) {
+      return next();
+    }
+    return res.status(401).json({ error: "Invalid token" });
   } catch (error) {
-    res.status(401).json({ error: "Invalid token" });
+    log(error);
+    return res.status(401).json({ error: "Invalid tolken" });
   }
 }
