3d025353eaf30637a095d47c263252c8195860dc
mamamamad <mamadhub8@gmail.com>
2025-08-21 14:32:24 +0330

    add new route to swagger.

--- swagger-docs/userLogin.mjs
+++ swagger-docs/userLogin.mjs
@@ -119,4 +119,145 @@ export default {
       },
     },
   },
+  "/api/Auth/logout": {
+    post: {
+      tags: ["Users"],
+      summary: "logout",
+      description: `logout
+      Codes =>
+         0 -> fail operations  
+         1 -> success operations
+      `,
+      produces: ["application/json"],
+      parameters: [
+        {
+          name: "RefreshToken",
+          in: "formData",
+          description: "Must be 80 char",
+          required: false,
+          type: "string",
+        },
+      ],
+      responses: {
+        200: {
+          description: "successful",
+        },
+      },
+    },
+  },
+  "/api/Profile/": {
+    get: {
+      tags: ["Profile"],
+      summary: "get Profile",
+      description:
+        "get Profile\nCodes =>\n0 -> fail operations\n1 -> success operations",
+      parameters: [
+        {
+          name: "Authorization",
+          in: "header",
+          description: "JWT token",
+          required: false,
+          schema: {
+            type: "string",
+          },
+        },
+      ],
+      responses: {
+        200: {
+          description: "Successful",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  code: { type: "integer" },
+                  msg: { type: "string" },
+                  avatar: { type: "string" },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  "/api/Profile/uploadProfilePicture": {
+    put: {
+      tags: ["Profile"],
+      summary: "Upload Profile",
+      description:
+        "Upload Profile\nCodes =>\n0 -> fail operations\n1 -> success operations",
+      parameters: [
+        {
+          name: "Authorization",
+          in: "header",
+          description: "JWT token",
+          required: false,
+          schema: {
+            type: "string",
+          },
+        },
+        {
+          name: "Profil",
+          in: "formData",
+          description: "upload Profile",
+          required: false,
+          type: "file",
+        },
+      ],
+      responses: {
+        200: {
+          description: "Successful",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  code: { type: "integer" },
+                  msg: { type: "string" },
+                  avatar: { type: "string" },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  "/api/Profile/deleteProfilePicture": {
+    delete: {
+      tags: ["Profile"],
+      summary: "delete Profile",
+      description:
+        "delete Profile\nCodes =>\n0 -> fail operations\n1 -> success operations",
+      parameters: [
+        {
+          name: "Authorization",
+          in: "header",
+          description: "JWT token",
+          required: false,
+          schema: {
+            type: "string",
+          },
+        },
+      ],
+      responses: {
+        200: {
+          description: "Successful",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  code: { type: "integer" },
+                  msg: { type: "string" },
+                  avatar: { type: "string" },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 };
