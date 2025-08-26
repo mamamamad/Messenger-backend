48d5941ad4acfdddb60b53649f51d4093de422d9
mamamamad <mamadhub8@gmail.com>
2025-08-26 14:12:40 +0330

    add message model

--- globalMoudles.mjs
+++ globalMoudles.mjs
@@ -1,6 +1,16 @@
 import Mondb from "./core/mongoDb.mjs";
 import Usermodel from "./model/userModel.mjs";
+import messageModel from "./model/messageModel.mjs";
+import QueueWorker from "./workers/QueueWorker.mjs";
 const MongoObjecctdb = new Mondb();
 const userModelObj = new Usermodel();
+const messageModelObj = new messageModel();
+const messageQueue = new QueueWorker("Message");
+await messageQueue.saveMessageProcces();
 
-export { MongoObjecctdb as MongoDb, userModelObj as UserModel };
+export {
+  MongoObjecctdb as MongoDb,
+  userModelObj as UserModel,
+  messageQueue,
+  messageModelObj as messageModel,
+};
